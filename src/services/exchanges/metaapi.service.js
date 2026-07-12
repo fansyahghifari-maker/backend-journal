2b84a9259d46b0e16159b8bcd56c6fa13767a00d
fansyahghifari-maker <fansyahghifari@gmail.com>
2026-07-12 16:22:13 +0700

    ke 14x

--- src/services/exchanges/metaapi.service.js
+++ src/services/exchanges/metaapi.service.js
@@ -67,7 +67,7 @@ const provisionAccount = async ({ loginNumber, password, serverName, platform, a
     platform,
     magic:    0,
     manualTrades: true,
-    reliability: 'high',
+    reliability: 'standar',
     application: 'MetaApi',
   }
 
